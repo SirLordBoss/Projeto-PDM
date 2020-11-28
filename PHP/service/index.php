709e4d8d1b9e5ad3ed49125088656f41577a6600
tiagoxalmeida <talmeida.pro@gmail.com>
2020-11-28 14:40:37 +0000

    Changes to be committed: 	new file:   PHP/index.html 	new file:   PHP/service/index.php 	new file:   queries.docx

---
+++ PHP/service/index.php
@@ -0,0 +1,12 @@
+<?php
+
+    print_r($_POST);
+    $dbusername = "epiz_27310703";
+    $dbpassword = ""
+
+    switch ($_POST['q']){
+        case 1:
+            echo 'O query e o 1';
+        break;
+    }
+        
