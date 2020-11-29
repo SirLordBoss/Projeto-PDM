82c3e0200493b1bb4ee8836e32a7eb65f9b6b32c
FIXOLAZ <fixolaz@hotmail.com>
2020-11-29 17:16:21 +0000

    Merge branch 'main' of https://github.com/SirLordBoss/Projeto-PDM into main
    # Conflicts:
    #	TitchersFriend/app/src/main/res/layout-xxxhdpi/activity_geriralunos.xml

---
+++ TitchersFriend/app/src/main/java/pt/ubi/di/pdm/titchersfriend/MarcarFaltas.java
@@ -0,0 +1,16 @@
+package pt.ubi.di.pdm.titchersfriend;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MarcarFaltas extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_marcarfaltas);
+    }
+
+
+
+}
