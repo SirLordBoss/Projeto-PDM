7a46cefccf8e99eedfbc4d5622e0174f9cf95a97
Dmrv2000 <Dmrv2000@outlook.pt>
2020-11-29 17:10:54 +0000

    Iniciação de algumas classes... nada de importante

---
+++ TitchersFriend/app/src/main/java/pt/ubi/di/pdm/titchersfriend/GerirAulas.java
@@ -0,0 +1,14 @@
+package pt.ubi.di.pdm.titchersfriend;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GerirAulas extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_geriralunos);
+
+    }
+}
