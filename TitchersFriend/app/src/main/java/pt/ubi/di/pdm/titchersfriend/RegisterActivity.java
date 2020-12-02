29057cba0cf119b211bdcdba35a3f619687f1377
Chaseroffice <64603836+Chaseroffice@users.noreply.github.com>
2020-12-02 17:19:16 +0000

    Update RegisterActivity.java

--- TitchersFriend/app/src/main/java/pt/ubi/di/pdm/titchersfriend/RegisterActivity.java
+++ TitchersFriend/app/src/main/java/pt/ubi/di/pdm/titchersfriend/RegisterActivity.java
@@ -8,7 +8,39 @@ import android.view.*;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.concurrent.ExecutionException;
+
 public class RegisterActivity extends AppCompatActivity {
+    public static String getM5(String input) {
+        try {
+
+            // Static getInstance method is called with hashing MD5
+            MessageDigest md = MessageDigest.getInstance("MD5");
+
+            // digest() method is called to calculate message digest
+            //  of an input digest() return array of byte
+            byte[] messageDigest = md.digest(input.getBytes());
+
+            // Convert byte array into signum representation
+            BigInteger no = new BigInteger(1, messageDigest);
+
+            // Convert message digest into hex value
+            String hashtext = no.toString(16);
+            while (hashtext.length() < 32) {
+                hashtext = "0" + hashtext;
+            }
+            return hashtext;
+        }
+
+        // For specifying wrong message digest algorithms
+        catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     Button btnCriar;
     EditText inputpass,inputEmail,inputRepPass,inputUser;
 
@@ -23,11 +55,45 @@ public class RegisterActivity extends AppCompatActivity {
         inputRepPass = (EditText) findViewById(R.id.inputRepPass);
         inputUser = (EditText) findViewById(R.id.inputUser);
 
+        Intent iCameFromActivity1 = getIntent() ;
+
+
         btnCriar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(RegisterActivity.this,HomePageEduc.class);
-                startActivity(i);
+                String x ="false";
+                String us = inputUser.getText().toString();
+                String pass = inputpass.getText().toString();
+                String passr = inputRepPass.getText().toString();
+                String email = inputEmail.getText().toString();
+                if(!passr.equals(pass)){
+                    Intent A2 = new Intent(RegisterActivity.this,RegisterActivity.class);
+
+                    startActivity(A2) ;
+                    Toast.makeText(RegisterActivity.this,"Palavras-passe diferem",Toast.LENGTH_LONG).show();
+                }
+                String enc = getM5(pass);
+
+                try {
+                    x = new Sender(RegisterActivity.this,"http://teachersfriend.ddns.net/service.php","50","u="+us+"&p="+enc+"&e="+email).execute().get();
+                } catch (ExecutionException e) {
+
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+
+                    e.printStackTrace();
+                }
+                if(x.contains("true")){
+                    Intent A2 = new Intent(RegisterActivity.this,MainActivity.class);
+                    startActivity(A2) ;
+                    Toast.makeText(RegisterActivity.this,"Sucesso na criação da conta",Toast.LENGTH_LONG).show();
+                }
+                if(x.contains("false")){
+                    Intent A2 = new Intent(RegisterActivity.this,MainActivity.class);
+                    startActivity(A2) ;
+                    Toast.makeText(RegisterActivity.this,"Erro na criação da conta",Toast.LENGTH_LONG).show();
+                }
+
             }
         });
     }
