52cd5f03eccae2507b222cf7fd3b7d336df8f3ab
Dmrv2000 <Dmrv2000@outlook.pt>
2020-12-07 11:42:56 +0000

    algumas alterações

--- TitchersFriend/app/src/main/java/pt/ubi/di/pdm/titchersfriend/GerirAulas.java
+++ TitchersFriend/app/src/main/java/pt/ubi/di/pdm/titchersfriend/GerirAulas.java
@@ -1,14 +1,27 @@
 package pt.ubi.di.pdm.titchersfriend;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class GerirAulas extends AppCompatActivity {
 
+    Button sumario;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_geriralunos);
+        setContentView(R.layout.activity_geriraulas);
+
+        sumario = (Button)findViewById(R.id.btnAddAula);
+        sumario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(GerirAulas.this,Sumarios.class);
+                startActivity(i);
+            }
+        });
 
     }
 }
