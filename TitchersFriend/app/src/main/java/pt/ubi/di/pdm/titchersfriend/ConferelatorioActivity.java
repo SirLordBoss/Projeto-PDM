26309d6c1741ee92d4d9ebecd0e55355b8e14063
tiagoxalmeida <talmeida.pro@gmail.com>
2020-12-07 13:41:11 +0000

    Example Commit

--- TitchersFriend/app/src/main/java/pt/ubi/di/pdm/titchersfriend/ConferelatorioActivity.java
+++ TitchersFriend/app/src/main/java/pt/ubi/di/pdm/titchersfriend/ConferelatorioActivity.java
@@ -6,11 +6,14 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Typeface;
 import android.graphics.pdf.PdfDocument;
 import android.os.Bundle;
 import android.os.Environment;
 import android.view.View;
 import android.widget.Button;
+
+import androidx.core.content.res.ResourcesCompat;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -37,13 +40,18 @@ public class ConferelatorioActivity extends Activity {
         PdfDocument myDocument = new PdfDocument();
         Paint myPaint = new Paint();
 
-        PdfDocument.PageInfo myDocumentInfo = new PdfDocument.PageInfo.Builder(250,400,1).create();
+        PdfDocument.PageInfo myDocumentInfo = new PdfDocument.PageInfo.Builder(595,842,1).create();
+        //dimensões em postscript de uma página A4 - 595x842
         PdfDocument.Page myPage1 = myDocument.startPage(myDocumentInfo);
         Canvas cnvs = myPage1.getCanvas();
         Bitmap bitmap = BitmapFactory.decodeResource(getApplicationContext().getResources(), R.drawable.fundo);
-        Bitmap scaled = Bitmap.createScaledBitmap(bitmap,250,400,false);
+        Bitmap scaled = Bitmap.createScaledBitmap(bitmap,595,842,false);
+        //fundo da folha compatível com o fundo da app
+
+        Typeface plain = ResourcesCompat.getFont(getApplicationContext(), R.font.kindergarten);
+        myPaint.setTypeface(plain);
         cnvs.drawBitmap(scaled,0,0,myPaint);
-        cnvs.drawText(getString(R.string.app_name),40,50,myPaint);
+        cnvs.drawText(getString(R.string.app_name),20,50,myPaint);
 
         myDocument.finishPage(myPage1);
 
