5a8bf1e79e0dc83d84ca6310a1c5f63310f160d3
SirLordBoss <ruicarrilho9@hotmail.com>
2020-12-09 17:47:20 +0000

    Update HomePageAdmin.java

--- TFAdmin/app/src/main/java/pt/ubi/di/pdm/titchersfriend/HomePageAdmin.java
+++ TFAdmin/app/src/main/java/pt/ubi/di/pdm/titchersfriend/HomePageAdmin.java
@@ -8,55 +8,51 @@ import android.widget.ImageButton;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class HomePageAdmin extends AppCompatActivity {
-   
-   ImageButton gerirAlunos,gerirAulas,sumario,faltas;
+
+   ImageButton gerir_alunos_button, gerir_aulas_button, mudar_pass_button, gerir_educ_button;
 
    protected void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
       setContentView(R.layout.activity_homepageadmin);
 
-      gerirAlunos = (ImageButton)findViewById(R.id.gerirAlunos) ;
-      gerirAulas = (ImageButton)findViewById(R.id.gerirAulas) ;
-      sumario = (ImageButton)findViewById(R.id.sumario) ;
-      faltas = (ImageButton)findViewById(R.id.faltas) ;
+      gerir_alunos_button = (ImageButton)findViewById(R.id.gerirAlunos);
+      gerir_aulas_button = (ImageButton)findViewById(R.id.gerirAulas);
+      mudar_pass_button = (ImageButton)findViewById(R.id.mudarPass);
+      gerir_educ_button = (ImageButton) findViewById(R.id.gerirEduc);
 
-      gerirAlunos.setOnClickListener(new View.OnClickListener() {
+      gerir_alunos_button.setOnClickListener(new View.OnClickListener() {
           @Override
           public void onClick(View v) {
-              Intent A2 = new Intent(HomePageEduc.this,GerirAlunos.class);
-              startActivity(A2) ;
+              Intent gerir_alunos = new Intent(HomePageAdmin.this, GerirAlunos.class);
+              startActivity(gerir_alunos);
           }
       });
 
-      gerirAulas.setOnClickListener(new View.OnClickListener() {
+      gerir_aulas_button.setOnClickListener(new View.OnClickListener() {
          @Override
          public void onClick(View v) {
-            Intent A2 = new Intent(HomePageEduc.this,GerirAulas.class);
-            startActivity(A2) ;
+            Intent gerir_aulas = new Intent(HomePageAdmin.this, GerirAulas.class);
+            startActivity(gerir_aulas);
          }
       });
 
-      sumario.setOnClickListener(new View.OnClickListener() {
-         
-         @Override
-         public void onClick(View v) {
-            Intent A2 = new Intent(HomePageEduc.this,Sumarios.class);
-            startActivity(A2) ;
-         }
-      });
-
-      faltas.setOnClickListener(new View.OnClickListener() {
+      mudar_pass_button.setOnClickListener(new View.OnClickListener() {
           @Override
-            public void onClick(View v) {
+          public void onClick(View v) {
+              //é para mudar a pass aqui, ou para fazer uma atividade dedicada?
+              //Intent mudar_pass = new Intent(HomePageAdmin.this, GerirAulas.class);
+              //startActivity(mudar_pass);
+          }
+      });
 
-                Intent A2 = new Intent(HomePageEduc.this,MarcarFaltas.class);
-                startActivity(A2) ;
-
-
-            }
-        });
-
-    }
-
+      gerir_educ_button.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View v) {
+              Log.v("DEBUG", "starting the intent");
+              Intent gerir_educ = new Intent(HomePageAdmin.this, GerirEduc.class);
+              startActivity(gerir_educ);
+          }
+      });
+   }
 }
 
