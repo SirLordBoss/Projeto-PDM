06567480be496cfb020d1de7a4fd71d122cef094
Dmrv2000 <Dmrv2000@outlook.pt>
2020-12-09 16:08:41 +0000

    debug

--- TitchersFriend/app/src/main/java/pt/ubi/di/pdm/titchersfriend/EditAlunoActivity.java
+++ TitchersFriend/app/src/main/java/pt/ubi/di/pdm/titchersfriend/EditAlunoActivity.java
@@ -6,8 +6,11 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,9 +18,11 @@ import androidx.appcompat.app.AppCompatActivity;
 public class EditAlunoActivity extends AppCompatActivity {
     DBHelper dbHelper;
     SQLiteDatabase base;
-    EditText nome,idade,sexo,morada,contacto;
+    EditText nome,idade,morada,contacto;
+    Spinner sexo;
     Button submeter,cancelar;
     String id = "";
+    int a1;
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
@@ -34,9 +39,26 @@ public class EditAlunoActivity extends AppCompatActivity {
 
         nome = (EditText) findViewById(R.id.editNome);
         idade = (EditText) findViewById(R.id.editIdade);
-        sexo = (EditText) findViewById(R.id.editSexo);
+        sexo = (Spinner) findViewById(R.id.editSexo);
         morada = (EditText) findViewById(R.id.editMorada);
         contacto = (EditText) findViewById(R.id.editContacto);
+
+        String[] items = new String[]{"Sexo: Feminino", "Sexo: Masculino"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(EditAlunoActivity.this, android.R.layout.simple_spinner_dropdown_item, items);
+        sexo.setAdapter(adapter);
+        sexo.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                a1 = sexo.getSelectedItemPosition();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+
+        });
+
 
         Cursor cursor =base.query(dbHelper.TABLE_NAME1,new String[]{"*"},null,null,null,null,null);
         while (cursor.moveToNext()){
@@ -46,10 +68,11 @@ public class EditAlunoActivity extends AppCompatActivity {
                 idade.setText(cursor.getString(cursor.getColumnIndex(dbHelper.COL3_T1)));
 
                 String Ssexo = cursor.getString(cursor.getColumnIndex(dbHelper.COL5_T1));
+
                 if(Ssexo.equals("1"))
-                    sexo.setText("Sexo: Masculino");
+                    sexo.setSelection(1);
                 else
-                    sexo.setText("Sexo: Feminino");
+                    sexo.setSelection(0);
 
                 morada.setText(cursor.getString(cursor.getColumnIndex(dbHelper.COL4_T1)));
                 contacto.setText(cursor.getString(cursor.getColumnIndex(dbHelper.COL6_T1)));
@@ -67,7 +90,7 @@ public class EditAlunoActivity extends AppCompatActivity {
                 oCV.put(dbHelper.COL2_T1,nome.getText().toString());
                 oCV.put(dbHelper.COL3_T1,idade.getText().toString());
                 oCV.put(dbHelper.COL4_T1,morada.getText().toString());
-                oCV.put(dbHelper.COL5_T1,sexo.getText().toString());
+                oCV.put(dbHelper.COL5_T1,String.valueOf(a1));
                 oCV.put(dbHelper.COL6_T1,contacto.getText().toString());
 
 
