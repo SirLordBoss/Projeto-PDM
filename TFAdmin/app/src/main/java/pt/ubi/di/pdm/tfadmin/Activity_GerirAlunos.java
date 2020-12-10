4a2c9685f629e466ed1c4e91fdf833245a0f975c
Dmrv2000 <73715121+Dmrv2000@users.noreply.github.com>
2020-12-10 14:35:50 +0000

    Update Activity_GerirAlunos.java

--- TFAdmin/app/src/main/java/pt/ubi/di/pdm/tfadmin/Activity_GerirAlunos.java
+++ TFAdmin/app/src/main/java/pt/ubi/di/pdm/tfadmin/Activity_GerirAlunos.java
@@ -1,12 +1,88 @@
 package pt.ubi.di.pdm.tfadmin;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Activity_GerirAlunos extends AppCompatActivity {
+
+    DBHelper dbHelper;
+    SQLiteDatabase base;
+    LinearLayout oLL;
+    Cursor oCursor,cursor2;
+    String tk;
+    int e_id,id;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_geriralunos);
+
+        Intent Cheguei = getIntent();
+        e_id = Integer.parseInt(Cheguei.getStringExtra("id"));
+
+        SharedPreferences shp = getApplicationContext().getSharedPreferences("important_variables",0);
+        id = shp.getInt("id",999);
+
+        Cursor cursor = base.query(dbHelper.TEDUCADOR,new String[]{"*"},null,null,null,null,null);
+        while (cursor.moveToNext()){
+            String c1 =cursor.getString(cursor.getColumnIndex(dbHelper.COL1_TEDUCADOR));
+            if (c1.equals(e_id)){
+                tk = cursor.getString(cursor.getColumnIndex(dbHelper.COL7_TEDUCADOR));
+                break;
+            }
+        }
+
+
+        dbHelper = new DBHelper(this);
+        base = dbHelper.getWritableDatabase();
+
+        displayAlunos();
+    }
+
+    protected void onResume() {
+        super.onResume();
+        dbHelper.updateEducando(this,base,id,e_id,tk);
+    }
+
+    public void displayAlunos() {
+        oLL = (LinearLayout) findViewById(R.id.visualizar);
+        oCursor = base.query(dbHelper.TEDUCANDO, new String[]{"*"}, null, null, null, null, null, null);
+
+        boolean bCarryOn = oCursor.moveToFirst();
+        while (bCarryOn) {
+            LinearLayout oLL1 = (LinearLayout) getLayoutInflater().inflate(R.layout.linha_visualizar, null);
+            oLL1.setId(oCursor.getInt(0) * 10 + 3);
+
+            TextView T1 = (TextView) oLL1.findViewById(R.id.nomeAluno);
+            T1.setId(oCursor.getInt(0) * 10 + 2);
+            T1.setText(oCursor.getString(1));
+
+            TextView T2 = (TextView) oLL1.findViewById(R.id.idAluno);
+            T2.setId(oCursor.getInt(0) * 10 + 1);
+            T2.setText(oCursor.getString(0));
+
+            ImageButton B1 = (ImageButton)oLL1.findViewById(R.id.btnVerAluno);
+            B1.setId(oCursor.getInt(0) * 10);
+            B1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent i = new Intent(Activity_GerirAlunos.this,Activity_PerfAluno.class);
+                    i.putExtra("id",(v.getId())/10);
+                    startActivity(i);
+                }
+            });
+
+            oLL.addView(oLL1);
+            bCarryOn = oCursor.moveToNext();
+        }
+
     }
 }
