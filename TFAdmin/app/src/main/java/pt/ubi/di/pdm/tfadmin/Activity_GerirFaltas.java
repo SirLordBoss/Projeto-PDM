9d35cee19689bfa754828fd5763f953ba239595b
Dmrv2000 <73715121+Dmrv2000@users.noreply.github.com>
2020-12-10 12:55:29 +0000

    Create Activity_GerirFaltas.java

---
+++ TFAdmin/app/src/main/java/pt/ubi/di/pdm/tfadmin/Activity_GerirFaltas.java
@@ -0,0 +1,13 @@
+package pt.ubi.di.pdm.tfadmin;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Activity_GerirFaltas extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gerirfaltas);
+    }
+}
