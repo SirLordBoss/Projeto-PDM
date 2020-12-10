7b053cdb82e19bdfeb6e99e7f6f7e8a60bcc710a
tiagoxalmeida <talmeida.pro@gmail.com>
2020-12-10 11:29:33 +0000

    Changes to be committed: 	modified:   ../TFAdmin/app/src/main/java/pt/ubi/di/pdm/tfadmin/DBHelper.java

--- TFAdmin/app/src/main/java/pt/ubi/di/pdm/tfadmin/DBHelper.java
+++ TFAdmin/app/src/main/java/pt/ubi/di/pdm/tfadmin/DBHelper.java
@@ -28,6 +28,7 @@ public class DBHelper extends SQLiteOpenHelper {
     protected static final String COL5_TADMIN = "u_sexo";
     protected static final String COL6_TADMIN = "u_email";
     protected static final String CREATE_TADMIN = "CREATE TABLE IF NOT EXISTS "+TADMIN+ "("+COL1_TADMIN+" INTEGER NOT NULL,"+COL2_TADMIN+" VARCHAR(200) NOT NULL,"+COL3_TADMIN+" INTEGER NOT NULL,"+COL4_TADMIN+" VARCHAR(200) NOT NULL,"+COL5_TADMIN+" INTEGER NOT NULL,"+COL6_TADMIN+" VARCHAR(200) NOT NULL);";
+
     protected static final String TEDUCADOR = "educador";
     protected static final String COL1_TEDUCADOR = "u_id";
     protected static final String COL2_TEDUCADOR = "u_nome";
@@ -39,6 +40,41 @@ public class DBHelper extends SQLiteOpenHelper {
     protected static final String COL8_TEDUCADOR = "t_utilizada";
     protected static final String CREATE_TEDUCADOR = "CREATE TABLE IF NOT EXISTS "+TEDUCADOR+ "("+COL1_TEDUCADOR+" INTEGER NOT NULL,"+COL2_TEDUCADOR+" VARCHAR(200) NOT NULL,"+COL3_TEDUCADOR+" INTEGER NOT NULL,"+COL4_TEDUCADOR+" VARCHAR(200) NOT NULL,"+COL5_TEDUCADOR+" INTEGER NOT NULL,"+COL6_TEDUCADOR+" VARCHAR(200) NOT NULL,"+COL7_TEDUCADOR+" VARCHAR(300) NOT NULL,"+COL8_TEDUCADOR+" INTEGER NOT NULL);";
 
+    protected static final String TEDUCANDO = "educando";
+    protected static final String COL1_TEDUCANDO = "e_id";
+    protected static final String COL2_TEDUCANDO = "e_nome";
+    protected static final String COL3_TEDUCANDO = "e_idade";
+    protected static final String COL4_TEDUCANDO = "e_morada";
+    protected static final String COL5_TEDUCANDO = "e_sexo";
+    protected static final String COL6_TEDUCANDO = "e_contacto";
+    protected static final String CREATE_TEDUCANDO = "CREATE TABLE IF NOT EXISTS "+TEDUCANDO+ "("+COL1_TEDUCANDO+" INTEGER NOT NULL,"+COL2_TEDUCANDO+" VARCHAR(200) NOT NULL,"+COL3_TEDUCANDO+" INTEGER NOT NULL,"+COL4_TEDUCANDO+" VARCHAR(200) NOT NULL,"+COL5_TEDUCANDO+" INTEGER NOT NULL,"+COL6_TEDUCANDO+" VARCHAR(200) NOT NULL);";
+
+    protected static final String TALERGIA = "alergia";
+    protected static final String COL1_TALERGIA = "e_id";
+    protected static final String COL2_TALERGIA = "al_id";
+    protected static final String COL3_TALERGIA = "al_nome";
+    protected static final String CREATE_TALERGIA = "CREATE TABLE IF NOT EXISTS "+TALERGIA+ "("+COL1_TALERGIA+" INTEGER NOT NULL,"+COL2_TALERGIA+" INTEGER NOT NULL,"+COL3_TALERGIA+" VARCHAR(200) NOT NULL);";
+
+    protected static final String TATIVIDADE = "atividade";
+    protected static final String COL1_TATIVIDADE = "a_id";
+    protected static final String COL2_TATIVIDADE = "a_sumario";
+    protected static final String COL3_TATIVIDADE = "a_data";
+    protected static final String CREATE_TATIVIDADE = "CREATE TABLE IF NOT EXISTS "+TATIVIDADE+ "("+COL1_TATIVIDADE+" INTEGER NOT NULL,"+COL2_TATIVIDADE+" VARCHAR(300) NOT NULL,"+COL3_TATIVIDADE+" VARCHAR(20) NOT NULL);";
+
+    protected static final String TFALTA = "falta";
+    protected static final String COL1_TFALTA = "e_nome";
+    protected static final String COL2_TFALTA = "is_falta";
+    protected static final String CREATE_TFALTA = "CREATE TABLE IF NOT EXISTS "+TFALTA+ "("+COL1_TFALTA+" VARCHAR(200) NOT NULL,"+COL2_TFALTA+" INTEGER NOT NULL);";
+
+    protected static final String TRELATORIO = "relatorio";
+    protected static final String COL1_TRELATORIO = "e_nome";
+    protected static final String COL2_TRELATORIO = "r_comer";
+    protected static final String COL3_TRELATORIO = "r_dormir";
+    protected static final String COL4_TRELATORIO = "r_coment";
+    protected static final String COL5_TRELATORIO = "r_necessidades";
+    protected static final String COL6_TRELATORIO = "r_curativos";
+    protected static final String CREATE_TRELATORIO = "CREATE TABLE IF NOT EXISTS "+TATIVIDADE+ "("+COL1_TRELATORIO+" VARCHAR(200) NOT NULL,"+COL2_TRELATORIO+" INTEGER NOT NULL,"+COL3_TRELATORIO+" INTEGER NOT NULL,"+COL4_TRELATORIO+" VARCHAR(300) NOT NULL,"+COL5_TRELATORIO+" VARCHAR(20) NOT NULL"+COL6_TRELATORIO+" INTEGER NOT NULL);";
+
     public DBHelper(@Nullable Context context) {
         super(context, DB_NAME, null, DB_VERSION);
         c = context;
@@ -48,6 +84,8 @@ public class DBHelper extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(CREATE_TADMIN);
         db.execSQL(CREATE_TEDUCADOR);
+        db.execSQL(CREATE_TEDUCANDO);
+        db.execSQL(CREATE_TALERGIA);
     }
 
     @Override
@@ -57,13 +95,17 @@ public class DBHelper extends SQLiteOpenHelper {
     }
 
     //#200
-    //função upgradeEducador para meter no método onResume
-    public static int upgradeEducador(SQLiteDatabase db, int id){//-1 : Sem comunicação   0 : Erro da base de dados   1 : Tudo ok
+    /*
+    * função updateEducador para meter no método onResume
+    *
+    * @param id  id do administrador que pede os dados
+    */
+    public static int updateEducador(SQLiteDatabase db, int id){//-1 : Sem comunicação   0 : Erro da base de dados   1 : Tudo ok
         String s = null;
         try {
             s = new Sender(c,"200", "id="+id,null).execute().get();
             if(s == null){
-                return 0;
+                return -1;
             }
             JSONObject o = new JSONObject(s);
             if(!o.getBoolean("success")){
@@ -97,13 +139,17 @@ public class DBHelper extends SQLiteOpenHelper {
     }
 
     //#201
-    //função upgradeAdmin para meter no método onResume
-    public static int upgradeAdmin(SQLiteDatabase db, int id){ //-1 : Sem comunicação   0 : Erro da base de dados   1 : Tudo ok
+    /*
+     * função updateAdmin para meter no método onResume
+     *
+     * @param id  id do administrador que pede os dados
+     */
+    public static int updateAdmin(SQLiteDatabase db, int id){ //-1 : Sem comunicação   0 : Erro da base de dados   1 : Tudo ok
         String s = null;
         try {
             s = new Sender(c,"201", "id="+id,null).execute().get();
             if(s == null){
-                return 0;
+                return -1;
             }
             JSONObject o = new JSONObject(s);
             if(!o.getBoolean("success")){
@@ -134,6 +180,50 @@ public class DBHelper extends SQLiteOpenHelper {
             return -1;
         }
     }
+    //#202
+    /*
+     * função updateEducando para meter no método onResume
+     *
+     * @param id  id do administrador que pede os dados
+     * @param e_id id do educador a que a turma pertence
+     * @param t_token token da turma a que o educando pertence
+     *
+     */
+    public static int updateEducando(SQLiteDatabase db, int id,int e_id,int t_token){ //-1 : Sem comunicação   0 : Erro da base de dados   1 : Tudo ok
+        String s = null;
+        try {
+            s = new Sender(c,"202", "id="+id+"&e_id="+e_id+"&t="+t_token,null).execute().get();
+            if(s == null){
+                return -1;
+            }
+            JSONObject o = new JSONObject(s);
+            if(!o.getBoolean("success")){
+                Toast.makeText(c,o.getString("error"),Toast.LENGTH_SHORT).show();
+                return 1;
+            }
 
+            db.execSQL("DROP TABLE "+TEDUCANDO);
+            db.execSQL(CREATE_TEDUCANDO);
+            String table = o.getString("table");
+            String[] lines = table.split(";");
+            for (String line : lines) {
+                String[] col = line.split(",");
+                ContentValues cv = new ContentValues();
+                cv.put(COL1_TEDUCANDO, col[0]);
+                cv.put(COL2_TEDUCANDO, col[1]);
+                cv.put(COL3_TEDUCANDO, col[2]);
+                cv.put(COL4_TEDUCANDO, col[3]);
+                cv.put(COL5_TEDUCANDO, col[4]);
+                cv.put(COL6_TEDUCANDO, col[5]);
+                if (db.insert(TEDUCANDO, null, cv) == -1)
+                    return 0;
+                cv.clear();
+            }
+            return 1;
+        } catch (ExecutionException | InterruptedException | JSONException e) {
+            e.printStackTrace();
+            return -1;
+        }
+    }
 
 }
