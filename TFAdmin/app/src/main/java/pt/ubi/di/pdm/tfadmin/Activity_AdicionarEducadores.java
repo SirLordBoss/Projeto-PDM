e8ba4df2d09afa8ac426f16549604e5c3461a0f1
SirLordBoss <rpcarrilho5@gmail.com>
2020-12-12 22:16:25 +0000

    password educadores corrigida

--- TFAdmin/app/src/main/java/pt/ubi/di/pdm/tfadmin/Activity_AdicionarEducadores.java
+++ TFAdmin/app/src/main/java/pt/ubi/di/pdm/tfadmin/Activity_AdicionarEducadores.java
@@ -15,6 +15,9 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 
 public class Activity_AdicionarEducadores extends AppCompatActivity {
@@ -31,6 +34,32 @@ public class Activity_AdicionarEducadores extends AppCompatActivity {
     ArrayList<String> sexList = new ArrayList<>();
 
     Button registo, btn_submeter, btn_cancelar;
+
+    public static String getM5(String input) {
+        try {
+
+            // Static getInstance method is called with hashing MD5
+            MessageDigest md = MessageDigest.getInstance("MD5");
+
+            // digest() method is called to calculate message digest
+            //  of an input digest() return array of byte
+            byte[] messageDigest = md.digest(input.getBytes());
+
+            // Convert byte array into signum representation
+            BigInteger no = new BigInteger(1, messageDigest);
+
+            // Convert message digest into hex value
+            String hashtext = no.toString(16);
+            while (hashtext.length() < 32) {
+                hashtext = "0" + hashtext;
+            }
+
+            return hashtext;
+        }   // For specifying wrong message digest algorithms
+        catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -85,7 +114,7 @@ public class Activity_AdicionarEducadores extends AppCompatActivity {
                 int sexo = Integer.valueOf(sexo_dropdown_selection);
 
 
-                if(db_helper.addEducador( id, educador_nome, idade, mor, sexo, em, "pwd") == 1){
+                if(db_helper.addEducador( id, educador_nome, idade, mor, sexo, em, getM5("pwd")) == 1){
 
                     Toast.makeText(Activity_AdicionarEducadores.this, "Feito!", Toast.LENGTH_SHORT).show();
                 } else{
